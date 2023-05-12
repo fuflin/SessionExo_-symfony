28f046b474d4b2ea3112dd29c72ee97646d0819d
fuflin <abdelilah.fakhour@outlook.fr>
2023-05-12 11:44:55 +0200

    création formulaire stagiaire

---
+++ src/Form/FormStagiaireType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Stagiaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class FormStagiaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('birth_date', DateType::class)
+            ->add('gender', TextType::class)
+            ->add('city', TextType::class)
+            ->add('cp', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('phone_number', TextType::class)
+            ->add('sessions', EntityClass::class, [
+                'class' => Session::class,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stagiaire::class,
+        ]);
+    }
+}
